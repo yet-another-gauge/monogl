7d401169c98f1956aff07b2a27718ae9301846b5
Dyachenko Ivan <vandyachen@gmail.com>
2018-08-04 23:11:42 +0300

    implement monogl_surface_info_t
    monogl_surface_info_t describes information about monogl_surface_t;
    at the moment it's only dimension (width, height) and total count of pixels;

---
+++ include/monogl/monogl_types.h
@@ -0,0 +1,32 @@
+/*
+ * This file is part of the 'Yet another gauge' project.
+ *
+ * Copyright (C) 2018 Ivan Dyachenko <vandyachen@gmail.com>
+ *
+ * This library is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef YET_ANOTHER_GAUGE__MONOGL__TYPES_H
+#define YET_ANOTHER_GAUGE__MONOGL__TYPES_H
+
+#ifndef MONOGL_API
+#define MONOGL_API
+#endif
+
+/**
+ * @brief Describes information about dimensions
+ */
+typedef struct monogl_surface_info_t monogl_surface_info_t;
+
+#endif // YET_ANOTHER_GAUGE__MONOGL__TYPES_H
