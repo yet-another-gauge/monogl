dcfa17a094efa0db57ff986599f1045164d78908
Dyachenko Ivan <vandyachen@gmail.com>
2018-09-02 15:03:29 +0300

    write unit tests for monogl_image_t

--- tests/test_monogl_image.c
+++ tests/test_monogl_image.c
@@ -22,7 +22,7 @@
 
 #include "../include/monogl/monogl_image.h"
 
-void test_monogl_image_create(void) {
+void test_monogl_image_new(void) {
   uint16_t width = 132;
   uint16_t height = 64;
 
@@ -31,8 +31,64 @@ void test_monogl_image_create(void) {
 
   monogl_image_t *image = monogl_image_new(width, height, points);
 
-  TEST_ASSERT_EQUAL_UINT16(monogl_image_get_width(image), width);
-  TEST_ASSERT_EQUAL_UINT16(monogl_image_get_height(image), height);
+  TEST_ASSERT_NOT_NULL(image);
+  TEST_ASSERT_NOT_NULL(points);
+
+  monogl_image_delete(image);
+}
+
+void test_monogl_image_delete(void) {
+  uint16_t width = 132;
+  uint16_t height = 64;
+
+  size_t byte_size = (width * height + 7u) / 8u;
+  uint8_t *points = malloc(byte_size);
+
+  monogl_image_t *image = monogl_image_new(width, height, points);
+
+  monogl_image_delete(image);
+
+  TEST_ASSERT_NOT_NULL(points);
+}
+
+void test_monogl_image_get_width(void) {
+  uint16_t width = 128;
+  uint16_t height = 64;
+
+  size_t byte_size = (width * height + 7u) / 8u;
+  uint8_t *points = malloc(byte_size);
+
+  monogl_image_t *image = monogl_image_new(width, height, points);
+
+  TEST_ASSERT_EQUAL_UINT16(width, monogl_image_get_width(image));
+
+  monogl_image_delete(image);
+}
+
+void test_monogl_image_get_height(void) {
+  uint16_t width = 128;
+  uint16_t height = 64;
+
+  size_t byte_size = (width * height + 7u) / 8u;
+  uint8_t *points = malloc(byte_size);
+
+  monogl_image_t *image = monogl_image_new(width, height, points);
+
+  TEST_ASSERT_EQUAL_UINT16(height, monogl_image_get_height(image));
+
+  monogl_image_delete(image);
+}
+
+void test_monogl_image_get_points(void) {
+  uint16_t width = 128;
+  uint16_t height = 64;
+
+  size_t byte_size = (width * height + 7u) / 8u;
+  uint8_t *points = malloc(byte_size);
+
+  monogl_image_t *image = monogl_image_new(width, height, points);
+
+  TEST_ASSERT_EQUAL_MEMORY(points, monogl_image_get_points(image), byte_size);
 
   monogl_image_delete(image);
 }
@@ -40,7 +96,11 @@ void test_monogl_image_create(void) {
 int main(void) {
   UNITY_BEGIN();
 
-  RUN_TEST(test_monogl_image_create);
+  RUN_TEST(test_monogl_image_new);
+  RUN_TEST(test_monogl_image_delete);
+  RUN_TEST(test_monogl_image_get_width);
+  RUN_TEST(test_monogl_image_get_height);
+  RUN_TEST(test_monogl_image_get_points);
 
   return UNITY_END();
 }
